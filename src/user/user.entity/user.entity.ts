38000db8af351ed893d59b3cc8185dc8377523be
knarimatsu <nariomasahisa@gmail.com>
2024-09-02 22:03:12 +0900

    UserEntity作成

--- src/user/user.entity/user.entity.ts
+++ src/user/user.entity/user.entity.ts
@@ -1,1 +1,16 @@
-export class UserEntity {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true })
+  refreshToken: string;
+}
